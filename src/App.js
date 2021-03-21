c6d30e570619c2254f20a15fa7392af896851f61
Abubakar Diallo <dialloabubakar5861@gmail.com>
2021-03-21 19:07:25 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React from 'react';
 
-import Movies from "./components/movies";
+import Movies from './components/movies';
 
-import "./App.css";
+import './App.css';
+import { Redirect, Route, Switch } from 'react-router';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
+import NavBar from './components/navBar';
+import MovieForm from './components/movieForm';
+import LoginForm from './components/loginForm';
 
 function App() {
   return (
-    <main className='container'>
-      <Movies />
-    </main>
+    <>
+      <NavBar />
+      <main className='container'>
+        <Switch>
+          <Route path='/login' component={LoginForm} />
+          <Route path='/movies/:id' component={MovieForm} />
+          <Route path='/movies' component={Movies} />
+          <Route path='/customers' component={Customers} />
+          <Route path='/rentals' component={Rentals} />
+          <Route path='/not-found' component={NotFound} />
+          <Redirect from='/' to='/movies' exact />
+          <Redirect to='/not-found' />
+        </Switch>
+      </main>
+    </>
   );
 }
 
